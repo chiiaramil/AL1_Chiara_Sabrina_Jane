0041fec018818cddff6cff6a977acfb8c825ab6d
Sabrina Leuthard <sabrina_leuthard@sluz.ch>
2025-04-16 19:04:05 +0200

    Delete menuToggle.js

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleBtn = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector(".nav_link");
 
-  if (toggleBtn && navLinks) {
-    toggleBtn.addEventListener("click", () => {
-      navLinks.classList.toggle("open");
-    });
-  }
-});
